26207d1088487859bdc01242f8e0e02fd581e4ba
Anton Lovrić <80052812+antonlovric@users.noreply.github.com>
2021-11-16 15:01:31 -0800

    Postavljen kostur za 3 sekcije. Potrebno bolje pozicionirati elemente na hero sekciji kako bi bio olakšan responzivni razvoj. Prilagoditi Intro sekciju mobilnim uređajima i dovršiti carousel za Highlight sekciju

---
+++ scripts/script.js
@@ -0,0 +1,76 @@
+var swiper = new Swiper(".swiper", {
+  breakpoints:{
+    400:{
+      allowTouchMove: true,
+      slidesPerView: 1.2,
+      spaceBetween: 10,
+
+    },
+    600:{
+      allowTouchMove: true,
+      slidesPerView: 2.2,
+      spaceBetween: 10,
+    },
+    800:{
+      allowTouchMove: true,
+      slidesPerView: 3.2,
+      spaceBetween: 10,
+    },
+    1200:{
+      allowTouchMove: false,
+      slidesPerView: 4,
+
+    },
+  },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: 4,
+    spaceBetween: 0,
+    setWrapperSize: true,
+    allowSwipeToNext: false, allowSwipeToPrev: false, allowTouchMove: false
+  });
+
+  var highlightSwiper = new Swiper(".highlightSwiper", {
+    containerModifierClass: "highlightSwiper-",
+    slideClass: "highlightSwiper-slide",
+    wrapperClass: "highlightSwiper-wrapper",
+    // loop: true,
+    navigation: {
+      nextEl: ".navButtonRight",
+      prevEl: ".navButtonLeft",
+    },
+  })
+
+  console.log(highlightSwiper.slides);
+  console.log(swiper.slides);
+
+
+
+
+window.addEventListener("resize", updateSwiper);
+function updateSwiper()
+{
+  if (window.innerWidth > 600){
+     swiper.allowTouchMove = true;
+  }
+
+}
+
+const items = document.querySelectorAll(".carouselItem");
+
+
+items.forEach((item) => {
+  item.addEventListener("mouseenter", function(){
+    toggleCategory(item)
+  });
+  item.addEventListener("mouseleave", function(){
+    toggleCategory(item)
+  });
+})
+
+function toggleCategory(item){
+  item.classList.toggle("showCategory")
+  item.classList.toggle("hideCategory")
+}
