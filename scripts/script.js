f4c023982365c10d7503538009f20a2d49ad0068
Anton Lovrić <80052812+antonlovric@users.noreply.github.com>
2021-11-18 23:41:28 +0100

    Dodane slide animacije i promijenjeni fontovi

--- scripts/script.js
+++ scripts/script.js
@@ -1,100 +1,98 @@
-var swiper = new Swiper(".swiper", {
-  breakpoints:{
-    300:{
+var swiper = new Swiper('.swiper', {
+  breakpoints: {
+    300: {
       allowTouchMove: true,
       slidesPerView: 1.2,
       spaceBetween: 10,
-
     },
-    600:{
+    600: {
       allowTouchMove: true,
       slidesPerView: 2.2,
       spaceBetween: 10,
     },
-    800:{
+    800: {
       allowTouchMove: true,
       slidesPerView: 3.2,
       spaceBetween: 10,
     },
-    1200:{
+    1200: {
       allowTouchMove: false,
       slidesPerView: 4,
-
     },
   },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    slidesPerView: 4,
-    spaceBetween: 0,
-    setWrapperSize: true,
-    allowSwipeToNext: false, allowSwipeToPrev: false, allowTouchMove: false
-  });
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  slidesPerView: 4,
+  spaceBetween: 0,
+  setWrapperSize: true,
+  allowSwipeToNext: false,
+  allowSwipeToPrev: false,
+  allowTouchMove: false,
+});
 
-
-
-  var highlightSwiper = new Swiper(".highlightSwiper", {
-    breakpoints:{
-      300:{
-        allowTouchMove: true,
-        slidesPerView: 1.2,
-        spaceBetween:10,
-  
-      },
-      600:{
-        allowTouchMove: true,
-        slidesPerView: 2.2,
-        spaceBetween: 10,
-      },
-      800:{
-        allowTouchMove: true,
-        slidesPerView: 3,
-        spaceBetween: 10,
-      },
-    },
-    navigation: {
-      nextEl: ".navButtonRight",
-      prevEl: ".navButtonLeft",
-    },
-    loop:true,
-    slidesPerView: 3,
+var highlightSwiper = new Swiper('.highlightSwiper', {
+  breakpoints: {
+    300: {
+      allowTouchMove: true,
+      slidesPerView: 1.2,
       spaceBetween: 10,
+    },
+    600: {
+      allowTouchMove: true,
+      slidesPerView: 2.2,
+      spaceBetween: 10,
+    },
+    800: {
+      allowTouchMove: true,
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+  },
+  navigation: {
+    nextEl: '.navButtonRight',
+    prevEl: '.navButtonLeft',
+  },
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 10,
+});
 
-    })
-
-window.addEventListener("resize", updateSwiper);
-function updateSwiper()
-{
-  if (window.innerWidth > 600){
-     swiper.allowTouchMove = true;
+window.addEventListener('resize', updateSwiper);
+function updateSwiper() {
+  if (window.innerWidth > 600) {
+    swiper.allowTouchMove = true;
   }
 }
 
-const items = document.querySelectorAll(".carouselItem");
+const items = document.querySelectorAll('.carouselItem');
 items.forEach((item) => {
-  item.addEventListener("mouseenter", function(){
-    toggleCategory(item)
+  item.addEventListener('mouseenter', function () {
+    toggleCategory(item);
   });
-  item.addEventListener("mouseleave", function(){
-    toggleCategory(item)
+  item.addEventListener('mouseleave', function () {
+    toggleCategory(item);
   });
-  if (window.innerWidth < 500){
-    item.addEventListener("click", function(){
-      toggleCategory(item)
+  if (window.innerWidth < 500) {
+    item.addEventListener('click', function () {
+      toggleCategory(item);
     });
   }
-})
+});
 
-function toggleCategory(item){
-  item.classList.toggle("showCategory")
-  item.classList.toggle("hideCategory")
-  const hiddenTextContainer = item.querySelector(".hiddenTextContainer");
-  hiddenTextContainer.classList.toggle("showHiddenText")
-  hiddenTextContainer.classList.toggle("hideHiddenText")
+function toggleCategory(item) {
+  item.classList.toggle('showCategory');
+  item.classList.toggle('hideCategory');
+  const hiddenTextContainer = item.querySelector('.hiddenTextContainer');
+  hiddenTextContainer.classList.toggle('showHiddenText');
+  hiddenTextContainer.classList.toggle('hideHiddenText');
 }
 
-if (window.innerWidth > 650){
-  VanillaTilt.init(document.querySelectorAll(".slide"));
+if (window.innerWidth > 650) {
+  VanillaTilt.init(document.querySelectorAll('.slide'));
 }
 
+AOS.init({
+  duration: 600,
+});
